b8a45334da121473266f72b8a819a00fc0967b80
gurielamp <gurielamp@gmail.com>
2023-10-31 06:22:25 +0700

    create handle login

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -4,15 +4,17 @@ import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export default function DashboardPage() {
-  const { data: session, status } = useSession()
+  const { data: session, status }: { data: any; status: string } = useSession()
   const router = useRouter()
-  console.log(session)
-  console.log(status)
   useEffect(() => {
     if (status === "unauthenticated") {
       return router.push("/login")
+    } else {
+      if (session !== undefined && session?.user.role !== "admin") {
+        return router.push("/")
+      }
     }
-  }, [router, status])
+  }, [router, session, session?.user.role, status])
   return (
     <div className="w-full h-96 bg-gray-300 rounded-[12px] flex justify-center items-center">
       <h1>Dashboard</h1>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,28 @@
 "use client"
 
+import { signIn } from "next-auth/react"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 export default function Login() {
-  const handleLogin = (e: any) => {
+  const { push } = useRouter()
+  const handleLogin = async (e: any) => {
     e.preventDefault()
-    fetch("/api/auth/login", {
-      method: "POST",
-      body: JSON.stringify({
+    try {
+      const res = await signIn("credentials", {
+        redirect: false,
         email: e.currentTarget.email.value,
         password: e.currentTarget.password.value,
-      }),
-    })
+        callbackUrl: "/dashboard",
+      })
+      if (!res?.error) {
+        push("/dashboard")
+      } else {
+        console.log(res.error)
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
